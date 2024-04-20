3225e635dcec704ae98be25238ba66ece2d69305
ethflow <162606221+ethflow@users.noreply.github.com>
2024-04-20 17:44:02 +0200

    created new feature file

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from feature_01 import return_even
+
+original_list = [i for i in range(10)]
+
+even_list = return_even(original_list)
 
 st.write("it works, programming god")
