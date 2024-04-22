a20911123726f8d2ae8c0520c45b2fcb6b850cc1
ethflow <162606221+ethflow@users.noreply.github.com>
2024-04-22 16:36:20 +0200

    changes

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
-from feature_01 import return_even
+import weeklymenugenerator
 
-original_list = [i for i in range(10)]
 
-even_list = return_even(original_list)
+menu_generator = WeeklyMenuGenerator()
+print("Grouped weekly menu (Monday to Friday):")
+days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+for day, meals in zip(days_of_week, menu_generator.grouped_menu):
+    veg_meal, non_veg_meal = meals
+    print(f"{day}: Vegetarian meal - {veg_meal[1]}, Non-vegetarian meal - {non_veg_meal[1]}")
+
 
 st.write("it works, programming god")
-st.write(even_list)
+st.write(menu_generator)
 
 st.write("Works properly")
 st.write("Works for me too")
