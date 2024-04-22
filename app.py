5264c57fb76ae31c77a93d8556e8053b339f3b8f
ethflow <162606221+ethflow@users.noreply.github.com>
2024-04-22 16:56:31 +0200

    Translation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from menu_generator import WeeklyMenuGenerator
 
 # Title of the application
-st.title("Weekly Menu Generator")
+st.title("Wochenplan")
 
 # Create an instance of WeeklyMenuGenerator
 # Initialize the menu generator as a session state variable to persist across reruns
@@ -17,18 +17,17 @@ def generate_new_weekly_menu():
     return st.session_state.menu_generator.grouped_menu
 
 # Add a button that generates a new weekly menu when clicked
-if st.button("Generate New Weekly Menu"):
+if st.button("Nächste Woche"):
     grouped_menu = generate_new_weekly_menu()
 else:
     # Use the existing menu if the button has not been clicked
     grouped_menu = st.session_state.menu_generator.grouped_menu
 
 # Display the grouped weekly menu for Monday to Friday
-st.header("Grouped Weekly Menu (Monday to Friday):")
-days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+days_of_week = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag']
 for day, meals in zip(days_of_week, grouped_menu):
     veg_meal, non_veg_meal = meals
     st.subheader(day)
     # Display only the meal names for vegetarian and non-vegetarian meals
-    st.text(f"Vegetarian meal: {veg_meal[1]}")
-    st.text(f"Non-vegetarian meal: {non_veg_meal[1]}")
+    st.text(f"Vegetarisch: {veg_meal[1]}")
+    st.text(f"Nicht-vegetarisch: {non_veg_meal[1]}")
