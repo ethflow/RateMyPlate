8c732d066f72cca8d485cc2016323d394d33b9ac
ethflow <162606221+ethflow@users.noreply.github.com>
2024-04-24 15:31:26 +0200

    storing changes

--- rating.py
+++ rating.py
@@ -2,22 +2,38 @@ import requests
 import random
 
 class MealRatingSystem:
+    # Dictionary to store ratings for each meal ID
+    ratings_dict = {}
+
     def __init__(self, meal_id=None, meal_name=None):
         self.meal_id = meal_id
         self.meal_name = meal_name
-        self.ratings = self.generate_random_ratings() if meal_id else []
+
+        # If the meal ID exists in the dictionary, use the existing ratings list
+        if meal_id in MealRatingSystem.ratings_dict:
+            self.ratings = MealRatingSystem.ratings_dict[meal_id]
+        else:
+            # Otherwise, generate new random ratings and store them in the dictionary
+            self.ratings = self.generate_random_ratings() if meal_id else []
+            MealRatingSystem.ratings_dict[meal_id] = self.ratings
 
     def fetch_meal_data(self, meal_name):
         """Fetch meal data from TheMealDB API based on the meal name."""
         url = f"https://www.themealdb.com/api/json/v1/1/search.php?s={meal_name}"
         response = requests.get(url)
         data = response.json()
-        
+
         if data['meals']:
             meal_info = data['meals'][0]
             self.meal_id = meal_info['idMeal']
             self.meal_name = meal_info['strMeal']
-            self.ratings = self.generate_random_ratings()
+
+            # Use the existing ratings list if available, or generate new ratings
+            if self.meal_id in MealRatingSystem.ratings_dict:
+                self.ratings = MealRatingSystem.ratings_dict[self.meal_id]
+            else:
+                self.ratings = self.generate_random_ratings()
+                MealRatingSystem.ratings_dict[self.meal_id] = self.ratings
         else:
             raise ValueError(f"No meal found with the name {meal_name}")
 
@@ -30,6 +46,9 @@ class MealRatingSystem:
     def add_user_rating_to_list(self, user_rating):
         """Add a user rating to the list and calculate the new average for the meal."""
         self.ratings.append(user_rating)
+        # Store the updated ratings list in the dictionary
+        MealRatingSystem.ratings_dict[self.meal_id] = self.ratings
+        
         # Calculate the new average rating
         new_average = round(sum(self.ratings) / len(self.ratings), 1)
         return new_average
