92b1ae31ca18149869376a906bd876e50cee904b
ethflow <162606221+ethflow@users.noreply.github.com>
2024-04-24 14:33:16 +0200

    removed visualization and added input check

--- app.py
+++ app.py
@@ -3,44 +3,39 @@ import pandas as pd
 from menu_generator import WeeklyMenuGenerator
 from rating import MealRatingSystem
 
-# Titel der Anwendung
+# Title of the application
 st.title("Wochenplan")
 
-# Initialisiere den Menügenerator als Session State Variable, um ihn über Neuladungen hinweg zu behalten
+# Initialize the menu generator as a session state variable to persist across reloads
 if 'menu_generator' not in st.session_state:
     st.session_state.menu_generator = WeeklyMenuGenerator()
 
-# Generiere ein neues wöchentliches Menü, wenn der Button geklickt wird, oder verwende das vorhandene
+# Generate a new weekly menu when the button is clicked, or use the existing one
 if st.button("Nächste Woche"):
     st.session_state.menu_generator = WeeklyMenuGenerator()
 grouped_menu = st.session_state.menu_generator.grouped_menu
 
-# Zeige das gruppierte wöchentliche Menü von Montag bis Freitag an
+# Display the grouped weekly menu from Monday to Friday
 days_of_week = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag']
 for day, meals in zip(days_of_week, grouped_menu):
     veg_meal, non_veg_meal = meals
     st.subheader(day)
 
-    # Verarbeite jede Mahlzeitentyp (vegetarisch und nicht-vegetarisch)
+    # Process each meal type (vegetarian and non-vegetarian)
     for meal in [veg_meal, non_veg_meal]:
         meal_id, meal_name = meal
         rating_system = MealRatingSystem(meal_id)
 
-        # Zeige den Namen der Mahlzeit an
+        # Display the meal name
         st.text(f"{meal_name}")
 
-        # Eingabe für neue Bewertungen über Streamlit
+        # Input for new ratings using Streamlit
         user_rating = st.number_input("Bewerte dieses Gericht von 1 bis 6", min_value=1, max_value=6, step=1,
                                       key=f"{meal_id}_{day}")
         submit_button = st.button(f"Bewertung abgeben für {meal_name}", key=f"btn_{meal_id}_{day}")
-        if submit_button and 1 <= user_rating <= 6:
+        if submit_button:
             new_average_rating = rating_system.add_user_rating_to_list(user_rating)
             rating_system.save_ratings(rating_system.ratings)
 
-            # Visualisiere den aktualisierten Durchschnitt als Balkendiagramm direkt unter jedem Gericht
-            data = pd.DataFrame({'Durchschnittsbewertung': [new_average_rating]}, index=[meal_name])
-            st.bar_chart(data)
         elif submit_button:
             st.error("Bitte bewerte dein Gericht mit einer Zahl von 1 bis 6")
-
-
